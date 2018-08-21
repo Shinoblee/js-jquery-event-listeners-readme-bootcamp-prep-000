e3aafd039764ded2f29fa455ab61ef3b07bec286
Lee Simon <shinoblee@gmail.com>
2018-08-21 11:05:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt(){
 function pressIt(){
   $('document').on('keydown', function(key){
     if(key.which === 71){
-      alert('You have pressed G');
+      alert('You have pressed "G"');
     }
   });
 }
